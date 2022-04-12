bcf7d37c257c83c6d4c3b1bb9836067d5008df3e
harith <harith.kv47@gmail.com>
2022-04-12 10:47:29 +0530

    Updates

---
+++ precache-manifest.9e1baccd9cfcd9b0db581ce2824a33eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf402307e94f09990ee9685a88de023",
+    "url": "/Real-Time-Chat-Application/index.html"
+  },
+  {
+    "revision": "b52dcb3f3b873a42f19d",
+    "url": "/Real-Time-Chat-Application/static/css/main.4736beb7.chunk.css"
+  },
+  {
+    "revision": "d3b6b2190212629ff13b",
+    "url": "/Real-Time-Chat-Application/static/js/2.bffb7481.chunk.js"
+  },
+  {
+    "revision": "b52dcb3f3b873a42f19d",
+    "url": "/Real-Time-Chat-Application/static/js/main.d03cc2af.chunk.js"
+  },
+  {
+    "revision": "deb4d083b5fed8bfeb61",
+    "url": "/Real-Time-Chat-Application/static/js/runtime~main.493c7cfa.js"
+  }
+]);
