f71b57dfef0502b5fe5ae43ef36fc6dce28f3efe
harith <harith.kv47@gmail.com>
2022-04-12 10:42:13 +0530

    changes made

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'localhost:5000';
+const ENDPOINT = 'https://websocketbe.herokuapp.com/';//http:localhost:5000/
 
 let socket;
 
